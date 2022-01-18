509b0af7b09b1f8670db4ce71c62dbacb7e4ba07
GatienDuh <78294761+GatienDuh@users.noreply.github.com>
2022-01-18 16:42:21 +0100

    Hitbox Attaque + code attaque

--- bta/Assets/Scripts/Player.cs
+++ bta/Assets/Scripts/Player.cs
@@ -9,6 +9,9 @@ public class Player : MonoBehaviour
     [SerializeField] private float speed = 5.0f;
     [SerializeField] private float jumpSpeed = 8.0f;
     [SerializeField] private float gravity = 20.0f;
+    
+    public Transform attackPoint;
+    [SerializeField] private float attackRange = 5.0f;
 
     private float horizontalMove = 0f;
 
@@ -34,7 +37,7 @@ public class Player : MonoBehaviour
         }
         moveDirection.y -= gravity * Time.deltaTime;
 
-        /*if (Input.GetKeyDown(KeyCode.RightArrow))
+        if (Input.GetKeyDown(KeyCode.RightArrow))
         {
             Vector3 temp = transform.rotation.eulerAngles;
             temp.y = 0f;
@@ -46,11 +49,64 @@ public class Player : MonoBehaviour
             Vector3 temp = transform.rotation.eulerAngles;
             temp.y = 180f;
             transform.rotation = Quaternion.Euler(temp);
-        }*/
+        }
     }
 
     private void FixedUpdate()
     {
         characterController.Move(moveDirection * Time.deltaTime);
     }
+
+    //#Combat System
+
+
+
+    /*void Attack()
+    {      
+        StartCoroutine(AttackAnimation());      
+    }*/
+
+    private void OnDrawGizmosSelected()
+    {
+        if (attackPoint == null)
+        {
+            return;
+        }
+           
+        Gizmos.DrawWireSphere(attackPoint.position, attackRange);
+    }
+
+    /*IEnumerator AttackAnimation()
+    {
+        Debug.Log("Start : " + Time.time);
+        isAttacking = true;
+        animator.SetTrigger("Attack");
+        rb.constraints = RigidbodyConstraints2D.FreezePosition;
+
+        yield return new WaitForSeconds(0.6f);
+        
+        Collider2D[] Enemies = Physics2D.OverlapCircleAll(attackPoint.position, attackRange, enemyLayers);
+
+        foreach (Collider2D enemy in Enemies)
+        {
+                enemy.GetComponent<Animator>().SetTrigger("Hurt");
+                enemy.GetComponent<Dummy>().health -= 1;             
+        }
+
+        Collider2D[] Trees = Physics2D.OverlapCircleAll(attackPoint.position, attackRange, treeLayers);
+
+        foreach (Collider2D tree in Trees)
+        {
+            tree.GetComponent<Tree>().health -= 1;
+        }
+
+        rb.constraints = RigidbodyConstraints2D.None;
+        rb.constraints = RigidbodyConstraints2D.FreezeRotation;
+
+        yield return new WaitForSeconds(0.2f);
+
+        isAttacking = false;
+
+        Debug.Log("Finish : " + Time.time);
+    }*/
 }
