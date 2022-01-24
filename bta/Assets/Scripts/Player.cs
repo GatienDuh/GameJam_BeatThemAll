c2ef810d50b4107e4cf69868754f054137863cd5
GatienDuh <78294761+GatienDuh@users.noreply.github.com>
2022-01-24 16:35:59 +0100

    ? Bugs ?

--- bta/Assets/Scripts/Player.cs
+++ bta/Assets/Scripts/Player.cs
@@ -10,10 +10,17 @@ public class Player : MonoBehaviour
     [SerializeField] private float jumpSpeed = 8.0f;
     [SerializeField] private float gravity = 20.0f;
     
+<<<<<<< HEAD
+    public Transform attackPoint;
+    [SerializeField] private float attackRange = 5.0f;
+
+    private float horizontalMove = 0f;
+=======
     [SerializeField] private float attackRange = 5.0f;
     public Transform attackPoint;
     public bool isAttacking = false;
     public LayerMask enemyLayers;
+>>>>>>> 59ff5d22cc94474f87bcaf924de308bb82014402
 
     private Vector3 moveDirection = Vector3.zero;
 
@@ -29,7 +36,11 @@ public class Player : MonoBehaviour
     {
         if (characterController.isGrounded)
         {
+<<<<<<< HEAD
+            moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0.0f, 0);
+=======
             moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0.0f, 0.0f);
+>>>>>>> 59ff5d22cc94474f87bcaf924de308bb82014402
             moveDirection *= speed;
 
             if (Input.GetButton("Jump"))
@@ -52,11 +63,14 @@ public class Player : MonoBehaviour
             temp.y = 180f;
             transform.rotation = Quaternion.Euler(temp);
         }
+<<<<<<< HEAD
+=======
 
         if (Input.GetKeyDown(KeyCode.Mouse0) && isAttacking == false)
         {
             Attack();
         }
+>>>>>>> 59ff5d22cc94474f87bcaf924de308bb82014402
     }
 
     private void FixedUpdate()
@@ -66,6 +80,51 @@ public class Player : MonoBehaviour
 
     //#Combat System
 
+<<<<<<< HEAD
+
+
+    /*void Attack()
+    {      
+        StartCoroutine(AttackAnimation());      
+    }*/
+
+    private void OnDrawGizmosSelected()
+    {
+        if (attackPoint == null)
+        {
+            return;
+        }
+           
+        Gizmos.DrawWireSphere(attackPoint.position, attackRange);
+    }
+
+    /*IEnumerator AttackAnimation()
+    {
+        Debug.Log("Start : " + Time.time);
+        isAttacking = true;
+        animator.SetTrigger("Attack");
+        rb.constraints = RigidbodyConstraints2D.FreezePosition;
+
+        yield return new WaitForSeconds(0.6f);
+        
+        Collider2D[] Enemies = Physics2D.OverlapCircleAll(attackPoint.position, attackRange, enemyLayers);
+
+        foreach (Collider2D enemy in Enemies)
+        {
+                enemy.GetComponent<Animator>().SetTrigger("Hurt");
+                enemy.GetComponent<Dummy>().health -= 1;             
+        }
+
+        Collider2D[] Trees = Physics2D.OverlapCircleAll(attackPoint.position, attackRange, treeLayers);
+
+        foreach (Collider2D tree in Trees)
+        {
+            tree.GetComponent<Tree>().health -= 1;
+        }
+
+        rb.constraints = RigidbodyConstraints2D.None;
+        rb.constraints = RigidbodyConstraints2D.FreezeRotation;
+=======
     void Attack()
     {      
         StartCoroutine(AttackAnimation());      
@@ -91,10 +150,16 @@ public class Player : MonoBehaviour
 
         //rb.constraints = RigidbodyConstraints2D.None;
         //rb.constraints = RigidbodyConstraints2D.FreezeRotation;
+>>>>>>> 59ff5d22cc94474f87bcaf924de308bb82014402
 
         yield return new WaitForSeconds(0.2f);
 
         isAttacking = false;
+<<<<<<< HEAD
+
+        Debug.Log("Finish : " + Time.time);
+    }*/
+=======
         Debug.Log("End Attack Coroutine");
     }
 
@@ -109,4 +174,5 @@ public class Player : MonoBehaviour
 
         Gizmos.DrawWireSphere(attackPoint.position, attackRange);
     }
+>>>>>>> 59ff5d22cc94474f87bcaf924de308bb82014402
 }
